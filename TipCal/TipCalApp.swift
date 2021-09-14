8936487fff64c68819f177d5ab301ceaac9a9240
Mohammad Haque <hawk@iamhawk.net>
2021-09-14 00:29:11 -0500

    Initial Commit

---
+++ TipCal/TipCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalApp.swift
+//  TipCal
+//
+//  Created by Hawk on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct TipCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
