0b92ffebd89fd2a863fe478c3a367562e20e00c3
Hawk <hawk@iamhawk.net>
2021-09-14 01:09:14 -0500

    Added Logo

--- TipCal/ContentView.swift
+++ TipCal/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            //Background color
+            Color.black
+                .ignoresSafeArea()
+                .opacity(0.2)
+            
+            //Logo
+            Image("appLogo")
+            
+        }
     }
 }
 
