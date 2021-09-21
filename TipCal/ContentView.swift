a0a438312780a988c50d1ec45988e0a993c5d125
Hawk <hawk@iamhawk.net>
2021-09-21 11:48:23 -0500

    More UI Added
    Updated the screen with more UI elements.

--- TipCal/ContentView.swift
+++ TipCal/ContentView.swift
@@ -25,27 +25,23 @@ struct OvalTextFieldStyle: TextFieldStyle {
     }
 }
 
-struct TipButton: View {
-    var percent:Int = 0
-    
-    var body: some View{
-        Button(action: {
-            print("The percentage is \(percent) %")
-        }, label: {
-            ZStack {
-                Image("tipButtonBG")
-                Text(String(percent)+" %")
-            }
-        })
-    }
-}
-
-struct TipCalculator {
-    
+struct Tips {
+    let t1:Int = 5
+    let t2:Int = 10
+    let t3:Int = 15
+    let t4:Int = 20
 }
 
 struct ContentView: View {
     @State private var theBill = ""
+    @State private var showTipButtons = false
+    @State private var showNewBill = false
+    @State private var selectedTip = 0
+    
+    private func CalculateTip() -> Double{
+        
+        return 0.0
+    }
     
     
     var body: some View {
@@ -53,7 +49,7 @@ struct ContentView: View {
             //Background color
             Color.black
                 .ignoresSafeArea()
-                .opacity(0.2)
+                .opacity(0.1)
             
             VStack {
                 //Logo
@@ -75,19 +71,84 @@ struct ContentView: View {
                     .padding()
                     .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
                 
-                VStack {
-                    HStack {
-                        TipButton(percent: 5)
-                        TipButton(percent: 10)
+                if showTipButtons {
+                    VStack {
+                        HStack {
+                            Button(action: {
+                                showNewBill = true
+                            }, label: {
+                                ZStack {
+                                    Image("tipButtonBG")
+                                    Text(String(Tips().t1)+"%")
+                                        .fontWeight(.semibold)
+                                }
+                            })
+                            Button(action: {
+                                
+                            }, label: {
+                                ZStack {
+                                    Image("tipButtonBG")
+                                    Text(String(Tips().t2)+"%")
+                                        .fontWeight(.semibold)
+                                }
+                            })
+                        }
+                        HStack {
+                            Button(action: {
+                                
+                            }, label: {
+                                ZStack {
+                                    Image("tipButtonBG")
+                                    Text(String(Tips().t3)+"%")
+                                        .fontWeight(.semibold)
+                                }
+                            })
+                            Button(action: {
+                                
+                            }, label: {
+                                ZStack {
+                                    Image("tipButtonBG")
+                                    Text(String(Tips().t4)+"%")
+                                        .fontWeight(.semibold)
+                                }
+                            })
+                        }
                     }
-                    HStack {
-                        TipButton(percent: 15)
-                        TipButton(percent: 20)
+                    .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                }
+                
+                
+                //
+                //
+                //
+                if(showNewBill){
+                    ZStack(alignment: .center) {
+                        Color.blue
+                        VStack{
+                            Spacer()
+                            Text("+ $10.00").fontWeight(.heavy)
+                            Spacer()
+                            Divider()
+                                .frame(height:1)
+                                .background(Color.white)
+                                .opacity(0.5)
+                            Spacer()
+                            Text("$53.98").fontWeight(.heavy)
+                            Spacer()
+                        }
                     }
+                    .cornerRadius(15.0).padding()
+                    .shadow(radius: 4)
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
                 }
             }
         }.onTapGesture {
             hideKeyboard()
+            
+            if(theBill != ""){
+                showTipButtons = true
+            }
         }
     }
 }
